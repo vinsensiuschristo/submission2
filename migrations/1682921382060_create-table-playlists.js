1a762ebe50c349db947c4a70b016578cc2767665
vinsensiuschristo <hallavinsen@gmail.com>
2023-05-01 13:17:16 +0700

    create table playlist with foreign key to playlists owner column

---
+++ migrations/1682921382060_create-table-playlists.js
@@ -0,0 +1,26 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('playlists', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    name: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+    },
+  });
+
+  // memberikan constraint foreign key pada owner terhadap kolom id dari tabel users
+  pgm.addConstraint('playlists', 'fk_playlists.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('playlists');
+};
