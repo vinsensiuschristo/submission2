f2004ccb7e274decf99fbbd3ba247ee6b1cd4b1d
vinsensiuschristo <hallavinsen@gmail.com>
2023-05-01 11:26:24 +0700

    first commit

--- src/server.js
+++ src/server.js
@@ -1,51 +1,80 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const Hapi = require("@hapi/hapi");
+const Hapi = require('@hapi/hapi');
 
 // albums
-const albums = require("./api/albums");
-const AlbumsService = require("./service/postgres/AlbumService");
-const AlbumsValidator = require("./validator/albums");
+const albums = require('./api/albums');
+const AlbumsService = require('./service/postgres/AlbumService');
+const AlbumsValidator = require('./validator/albums');
 
 // songs
-const songs = require("./api/songs");
-const SongsService = require("./service/postgres/SongService");
-const SongsValidator = require("./validator/songs");
+const songs = require('./api/songs');
+const SongsService = require('./service/postgres/SongService');
+const SongsValidator = require('./validator/songs');
+
+// users
+const users = require('./api/users');
+const UsersService = require('./service/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
+// authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./service/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
 
 const init = async () => {
-    const albumsService = new AlbumsService();
-    const songsService = new SongsService();
+  const albumsService = new AlbumsService();
+  const songsService = new SongsService();
+  const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
 
-    const server = Hapi.server({
-        port: process.env.PORT,
-        host: process.env.HOST,
-        routes: {
-            cors: {
-                origin: ["*"],
-            },
-        },
-    });
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
 
-    // Plugin
-    await server.register([
-        {
-            plugin: albums,
-            options: {
-                service: albumsService,
-                validator: AlbumsValidator,
-            },
-        },
-        {
-            plugin: songs,
-            options: {
-                service: songsService,
-                validator: SongsValidator,
-            },
-        },
-    ]);
+  // Plugin
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumsService,
+        validator: AlbumsValidator,
+      },
+    },
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
+      },
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
+      },
+    },
+  ]);
 
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
